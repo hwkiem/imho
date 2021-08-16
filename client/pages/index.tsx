a91f314ffb767945ffaebb944ce9e75d7eef1752
hwkiem <39803077+hwkiem@users.noreply.github.com>
2021-08-16 13:39:53 -0700

    Plumbing (#5)
    * initial commit of client yarn workspace monorepo structure. Needs pg debugging
    
    * removing domain layer. We'll import entity classes directly to clientside.
    
    * adding entities as a sub project and adjusting apollo server plugins
    
    * adding entities and client updates to nested sub module
    
    * adding entities for each of our business objects
    
    * adjusting client to have navbar and map on homepage.
    
    * rearrange folders and configure apolloserver cors
    
    * temporarily setup with gql playground, all resolvers updated since googleAPI
    
    * cleanup implement base entities
    
    * adding basic ui for map and navbar
    
    * updating tsconfig relationships between entities and client layer
    
    * adjusting tsconfig emit regulations
    
    * final commit for client and initial drawer for create review before implementing codegen
    
    Co-authored-by: James Ryan <jamesryan@Jamess-MBP-2.fios-router.home>

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,34 @@
+import { AspectRatio, Grid, GridItem, Box } from "@chakra-ui/react";
+import { Residence } from "entities";
+import { Layout } from "../components/layout/layout";
+import { Map } from "../components/ui/map";
+
+const residences: Pick<Residence, "resID" | "full_address" | "coords">[] = [
+  {
+    resID: 11,
+    full_address: "18 Roosevelt Pl, Rockville Centre, NY 11570, USA",
+    coords: {
+      lat: 40.66482329999999,
+      lng: -73.6470927,
+    },
+  },
+  {
+    resID: 1,
+    full_address: "12 W 104th St #3e, New York, NY 10025, USA",
+    coords: {
+      lat: 40.7969087,
+      lng: -73.96190469999999,
+    },
+  },
+];
+
+const center = { lat: 40.7969087, lng: -73.96190469999999 };
+
 const Index = () => {
-    console.log("Index Page!");
   return (
-      <div>Index page!</div>
+    <Layout>
+      <Map residences={residences} center={center} />
+    </Layout>
   );
 };
 
