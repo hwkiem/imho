e3d223c20f136ead10e4bb20eb5141831663d398
jryan3211 <45048622+jryan3211@users.noreply.github.com>
2021-08-27 16:26:17 -0400

    Adjust review (#30)
    * i did it
    
    * 4real
    
    Co-authored-by: James Ryan <jamesryan@Jamess-MBP-2.fios-router.home>

--- api/src/types.ts
+++ api/src/types.ts
@@ -1,110 +1,110 @@
-import { Client } from '@googlemaps/google-maps-services-js'
-import { Request, Response } from 'express'
-import { ObjectType, Field, InputType, Int } from 'type-graphql'
-import { googleMapsHandler } from './DataSources/mapsAPI'
-import { postgresHandler } from './dataSources/postgres'
-import { ResidenceGQL } from './Residence/residence'
-import { ReviewGQL } from './Review/reviews'
-import { UserGQL } from './User/user'
+import { Client } from '@googlemaps/google-maps-services-js';
+import { Request, Response } from 'express';
+import { ObjectType, Field, InputType, Int } from 'type-graphql';
+import { googleMapsHandler } from './DataSources/mapsAPI';
+import { postgresHandler } from './dataSources/postgres';
+import { ResidenceGQL } from './Residence/residence';
+import { ReviewGQL } from './Review/reviews';
+import { UserGQL } from './User/user';
 
 export type MyContext = {
-    req: Request //& { session: Express.Session };
-    res: Response
-    client: Client
+    req: Request; //& { session: Express.Session };
+    res: Response;
+    client: Client;
     dataSources: {
-        pgHandler: postgresHandler
-        googleMapsHandler: googleMapsHandler
-    }
-}
+        pgHandler: postgresHandler;
+        googleMapsHandler: googleMapsHandler;
+    };
+};
 
 // @GRAPHQL
 // @INPUT
 @InputType() // subset of User used as filter values
 export class PartialUser implements Partial<UserGQL> {
     @Field({ nullable: true })
-    first_name?: string
+    first_name?: string;
     @Field({ nullable: true })
-    last_name?: string
+    last_name?: string;
 }
 
 @InputType()
 export class PartialReview implements Partial<ReviewGQL> {
     @Field({ nullable: true })
-    rating: number
+    rating: number;
 
     @Field({ nullable: true })
-    rent: number
+    rent: number;
 }
 
 @InputType()
 export class PartialResidence implements Partial<ResidenceGQL> {
     @Field({ nullable: true })
-    apt_num: string
+    apt_num: string;
     @Field({ nullable: true })
-    avg_rent: number
+    avg_rent: number;
     @Field({ nullable: true })
-    city: string
+    city: string;
     @Field({ nullable: true })
-    postal_code: string
+    postal_code: string;
     @Field({ nullable: true })
-    route: string
+    route: string;
     @Field({ nullable: true })
-    state: string
+    state: string;
 }
 
 @InputType()
 export class RegisterInput {
     @Field()
-    email: string
+    email: string;
     @Field()
-    first_name: string
+    first_name: string;
     @Field()
-    last_name: string
+    last_name: string;
     @Field()
-    password: string
+    password: string;
 }
 
 @InputType()
 export class LoginInput {
     @Field()
-    email: string
+    email: string;
     @Field()
-    password: string
+    password: string;
 }
 
 @InputType()
 export class ReviewQueryInput {
     @Field(() => [Int], { nullable: true })
-    reviews?: [number]
+    reviews?: [number];
 }
 
 @InputType()
 export class CreateResidenceInput {
     @Field()
-    google_place_id: string
+    google_place_id: string;
 }
 
 @InputType()
 export class WriteReviewInput {
     // input from frontend
     @Field()
-    google_place_id: string
+    google_place_id: string;
 
-    @Field()
-    rating: number
+    @Field({ nullable: true })
+    rating?: number;
 
-    @Field()
-    rent: number
+    @Field({ nullable: true })
+    rent?: number;
 
-    user_id: number
+    user_id: number;
 }
 
 export class WriteReviewArgs {
     // args for backend
-    res_id: number
-    user_id: number
-    rent: number
-    rating: number
+    res_id: number;
+    user_id: number;
+    rent?: number;
+    rating?: number;
 }
 
 // use this for getReviewById?
@@ -119,51 +119,51 @@ export class WriteReviewArgs {
 @ObjectType()
 export class FieldError {
     @Field()
-    field: string
+    field: string;
     @Field()
-    message: string
+    message: string;
 }
 
 @ObjectType()
 export class UserResponse {
     @Field(() => [FieldError], { nullable: true })
-    errors?: FieldError[]
+    errors?: FieldError[];
 
     @Field(() => [UserGQL], { nullable: true })
-    users?: UserGQL[]
+    users?: UserGQL[];
 }
 
 @ObjectType()
 export class ResidenceResponse {
     @Field(() => [FieldError], { nullable: true })
-    errors?: FieldError[]
+    errors?: FieldError[];
 
     @Field(() => [ResidenceGQL], { nullable: true })
-    residences?: ResidenceGQL[]
+    residences?: ResidenceGQL[];
 }
 
 @ObjectType()
 export class ReviewResponse {
     @Field(() => [FieldError], { nullable: true })
-    errors?: FieldError[]
+    errors?: FieldError[];
 
     @Field(() => [ReviewGQL], { nullable: true })
-    reviews?: ReviewGQL[]
+    reviews?: ReviewGQL[];
 }
 
 @ObjectType()
 export class Coords {
     @Field()
-    lat: number
+    lat: number;
     @Field()
-    lng: number
+    lng: number;
 }
 
 @ObjectType()
 export class PlaceIDResponse {
     @Field(() => FieldError, { nullable: true })
-    errors?: FieldError
+    errors?: FieldError;
 
     @Field({ nullable: true })
-    place_id?: string
+    place_id?: string;
 }
