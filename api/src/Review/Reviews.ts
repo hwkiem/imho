0bce19c1b21849d6119216f88ceab80364b8e681
James Ryan <jamesryan@Jamess-MBP-2.fios-router.home>
2021-08-27 16:06:40 -0400

    i did it

--- api/src/Review/Reviews.ts
+++ api/src/Review/Reviews.ts
@@ -1,23 +1,23 @@
-import { ObjectType, Field, Int, Float } from 'type-graphql'
-import { Review } from 'entities'
+import { ObjectType, Field, Int, Float } from 'type-graphql';
+import { Review } from 'entities';
 
 @ObjectType()
 export class ReviewGQL implements Review {
     @Field(() => Int)
-    res_id: number
+    res_id: number;
 
     @Field()
-    user_id: number
+    user_id: number;
 
-    @Field(() => Float)
-    rating: number
+    @Field(() => Float, { nullable: true })
+    rating?: number;
 
-    @Field()
-    rent: number
-
-    @Field(() => String)
-    created_at = new Date()
+    @Field({ nullable: true })
+    rent?: number;
 
     @Field(() => String)
-    updated_at = new Date()
+    created_at = new Date();
+
+    @Field(() => String)
+    updated_at = new Date();
 }
