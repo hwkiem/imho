0493c71ef7b176a3c40c08c455cb82f43e14a54c
hwkiem <hwkiem@gmail.com>
2021-09-01 14:34:52 -0400

    initial work on dashboard page

--- client/src/pages/profile.tsx
+++ client/src/pages/profile.tsx
@@ -1,17 +1,46 @@
+import { Box, Flex, Heading, Stack, useColorModeValue } from '@chakra-ui/react';
 import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
 import { Layout } from '../components/layout/layout';
-import { MeDocument, MeQuery, RegularUserFragment } from '../generated/graphql';
+import {
+    MeDocument,
+    MeQuery,
+    RegularReviewFragment,
+    RegularUserFragment,
+} from '../generated/graphql';
 import { initializeApollo } from '../lib/apollo';
 import { Page } from '../types/page';
 import { useIsAuth } from '../utils/useIsAuth';
 
 interface ProfileProps {
     me: RegularUserFragment;
+    reviews: RegularReviewFragment;
 }
 
 const Profile: Page<ProfileProps> = ({ me }) => {
     useIsAuth();
-    return <div>Hello {me.first_name}!</div>;
+    return (
+        <Flex minH={'100vh'} bg={'gray.50'}>
+            <Stack
+                w={'100%'}
+                direction={'column'}
+                spacing={8}
+                my={12}
+                py={12}
+                px={6}
+                mx={6}
+            >
+                <Heading>
+                    Hello {me.first_name}! Welcome to your dashboard.
+                </Heading>
+                <Box bg={'white'} w={'100%'} m={4} rounded={4} boxShadow={'sm'}>
+                    <Heading size={'sm'} m={4}>
+                        Welcome Home!
+                    </Heading>
+                </Box>
+                <Stack></Stack>
+            </Stack>
+        </Flex>
+    );
 };
 
 export const getServerSideProps = async ({
