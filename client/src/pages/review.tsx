7e3e6ce1450935cd32bc9faf0edc2e784d89af06
hwkiem <hwkiem@gmail.com>
2021-09-03 15:28:51 -0400

    adding retro review form basic

--- client/src/pages/review.tsx
+++ client/src/pages/review.tsx
@@ -1,12 +1,22 @@
-import { ReviewForm } from "../components/forms/review";
-import { Center } from "@chakra-ui/react";
+import { Center, Flex, Heading, Stack } from '@chakra-ui/react';
+import { RetroReview } from '../components/forms/retroreview';
 
 const Review: React.FC = () => {
-  return (
-    <Center>
-      <ReviewForm />;
-    </Center>
-  );
+    return (
+        <Flex minH={'100vh'} bg={'gray.50'}>
+            <Stack
+                w={'100%'}
+                direction={'column'}
+                spacing={8}
+                my={12}
+                py={12}
+                px={6}
+                mx={6}
+            >
+                <RetroReview />
+            </Stack>
+        </Flex>
+    );
 };
 
 export default Review;
