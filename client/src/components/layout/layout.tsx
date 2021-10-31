dd6abead729f790e57115c776ecb80e6d7c222cd
Michael Pedersen <mp@miped.dk>
2021-10-31 21:44:13 +0000

    Make children optional

--- client/src/components/layout/layout.tsx
+++ client/src/components/layout/layout.tsx
@@ -2,15 +2,15 @@ import { Box } from '@chakra-ui/react';
 import { NavBar } from '../section/navbar';
 
 interface LayoutProps {
-    children: React.ReactNode;
+    children?: React.ReactNode;
 }
 
-export const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
+export const Layout: React.FC<LayoutProps> = (props: LayoutProps) => {
     console;
     return (
         <Box height={'100vh'}>
             <NavBar />
-            <Box h={'100%'}>{children}</Box>
+            <Box h={'100%'}>{props.children}</Box>
         </Box>
     );
 };
