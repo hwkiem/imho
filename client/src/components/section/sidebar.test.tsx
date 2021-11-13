9bc21b8afe494e19df13f962332be0c82c961417
Michael Pedersen <mp@miped.dk>
2021-11-13 22:07:39 +0000

    Add test for sidebar

---
+++ client/src/components/section/sidebar.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Sidebar from './sidebar';
+
+describe('Sidebar', () => {
+    it('renders a heading', () => {
+        render(
+            <Sidebar>
+                <div />
+            </Sidebar>
+        );
+        expect(screen.getByRole('heading')).toBeInTheDocument();
+    });
+
+    it('has a refresh button', () => {
+        render(
+            <Sidebar>
+                <div />
+            </Sidebar>
+        );
+        expect(screen.getByRole('button')).toBeInTheDocument();
+    });
+
+    it('renders its children', () => {
+        render(
+            <Sidebar>
+                <div data-testid="my-child" />
+            </Sidebar>
+        );
+        expect(screen.getByTestId('my-child')).toBeInTheDocument();
+    })
+});
