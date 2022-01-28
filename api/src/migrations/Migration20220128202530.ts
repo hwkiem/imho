1bad12df79e856ce5bc40c318d135df4b525f497
James Ryan <jamesryan@Jamess-MBP-2.fios-router.home>
2022-01-28 15:26:38 -0500

    prettier

--- api/src/migrations/Migration20220128202530.ts
+++ api/src/migrations/Migration20220128202530.ts
@@ -1,19 +1,33 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20220128202530 extends Migration {
+    async up(): Promise<void> {
+        this.addSql(
+            'alter table "residence" drop constraint if exists "residence_unit_check";'
+        );
+        this.addSql(
+            'alter table "residence" alter column "unit" type varchar(255) using ("unit"::varchar(255));'
+        );
+        this.addSql(
+            'alter table "residence" alter column "unit" set default \'single family\';'
+        );
+        this.addSql(
+            'alter table "residence" add constraint "residence_place_id_unit_unique" unique ("place_id", "unit");'
+        );
+    }
 
-  async up(): Promise<void> {
-    this.addSql('alter table "residence" drop constraint if exists "residence_unit_check";');
-    this.addSql('alter table "residence" alter column "unit" type varchar(255) using ("unit"::varchar(255));');
-    this.addSql('alter table "residence" alter column "unit" set default \'single family\';');
-    this.addSql('alter table "residence" add constraint "residence_place_id_unit_unique" unique ("place_id", "unit");');
-  }
-
-  async down(): Promise<void> {
-    this.addSql('alter table "residence" drop constraint if exists "residence_unit_check";');
-    this.addSql('alter table "residence" alter column "unit" type varchar(255) using ("unit"::varchar(255));');
-    this.addSql('alter table "residence" alter column "unit" set default \'single\';');
-    this.addSql('alter table "residence" drop constraint "residence_place_id_unit_unique";');
-  }
-
+    async down(): Promise<void> {
+        this.addSql(
+            'alter table "residence" drop constraint if exists "residence_unit_check";'
+        );
+        this.addSql(
+            'alter table "residence" alter column "unit" type varchar(255) using ("unit"::varchar(255));'
+        );
+        this.addSql(
+            'alter table "residence" alter column "unit" set default \'single\';'
+        );
+        this.addSql(
+            'alter table "residence" drop constraint "residence_place_id_unit_unique";'
+        );
+    }
 }
